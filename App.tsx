37a341808ba5cba91496bda04f57c57883b5823f
sigongjoa <zeskywa499@gmail.com>
2025-07-31 17:41:09 +0900

    Update to working files

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import ThreadList from './components/ThreadList';
 import ThreadDetailView from './components/KanbanBoard';
 import InputModal from './components/InputModal';
 import ConversationViewerModal from './components/ExportModal';
-import useLocalStorage from './services/useLocalStorage';
 
 type ModalMode = 'addThread' | 'editThread';
 
@@ -19,8 +18,8 @@ interface ModalState {
 }
 
 const App: React.FC = () => {
-    const [threads, setThreads] = useLocalStorage<Thread[]>('threads', INITIAL_THREADS);
-  const [uploadedConversations, setUploadedConversations] = useLocalStorage<UploadedConversation[]>('conversations', []);
+  const [threads, setThreads] = useState<Thread[]>(INITIAL_THREADS);
+  const [uploadedConversations, setUploadedConversations] = useState<UploadedConversation[]>([]);
   
   const [activeThreadId, setActiveThreadId] = useState<string>('thread-1');
   const [viewingConversation, setViewingConversation] = useState<UploadedConversation | null>(null);
